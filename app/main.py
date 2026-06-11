5720b175ccea06680ca16452a942cb696c69338d
Shiva Pal <spal13138@gmail.com>
2026-06-11 15:39:23 +0530

    enable cors for backend integration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from app.database import Base,engine
 from app.routes import router
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI()
 
+origins=[
+    "http://127.0.0.1:5500/",
+    "https://clinexa-pauo.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(router.api_router)
